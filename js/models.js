3a27f417cc1843148e9a9272374e4fb9374658ec
Andres Rios <andresfrv1608@gmail.com>
2026-09-21 17:52:54 -0500

    feat: Actualizacion De Models

--- js/models.js
+++ js/models.js
@@ -5,33 +5,13 @@ class MultiAppModel {
         this.imagenVisible = true;
     }
 
-    // Lógica del Mensaje
-    actualizarMensaje(nuevoMensaje) {
-        this.mensaje = nuevoMensaje;
-    }
-    obtenerMensaje() {
-        return this.mensaje;
-    }
+    actualizarMensaje(nuevoMensaje) { this.mensaje = nuevoMensaje; }
+    obtenerMensaje() { return this.mensaje; }
 
-    // Lógica del Contador
-    incrementar() {
-        this.contador++;
-    }
-    disminuir() {
-        this.contador--;
-    }
-    reiniciar() {
-        this.contador = 0;
-    }
-    obtenerContador() {
-        return this.contador;
-    }
+    incrementar() { this.contador++; }
+    disminuir() { this.contador--; }
+    reiniciar() { this.contador = 0; }
+    obtenerContador() { return this.contador; }
 
-    // Lógica de Imagen
-    setVisibilidadImagen(estado) {
-        this.imagenVisible = estado;
-    }
-    obtenerVisibilidadImagen() {
-        return this.imagenVisible;
-    }
+    setVisibilidadImagen(estado) { this.imagenVisible = estado; }
 }
